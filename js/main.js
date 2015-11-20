296aaf7e7d49e4b244a6a8e7fceceabdc0230893
Jorge Luis Monroy <jlmonroy13@gmail.com>
2015-11-20 14:52:41 -0500

    Header OK

--- js/main.js
+++ js/main.js
@@ -1,58 +1,20 @@
-$(window).scroll(function(){
-	var body = $("body");
-	var top = body.scrollTop(); // Get position of the body
-	if (top > 650) {
-		$(".navbar2").addClass("blancoTransparente navbar-fixed-top");
-		$(".navbar2").fadeIn("slow");
-	}
-	else if (top < 651) {
-		$(".navbar2").removeClass("blancoTransparente navbar-fixed-top");
-		$(".navbar2").fadeOut("slow");
-	}
-});
+//==================================================================
+//www.solvingbooks.com / FrontEnd-Jorge Monroy / BackEnd-Jose Osorio
+//==================================================================
+// NAVIGATION HEADER
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
 
-
-$(document).ready(function(){
-	
-	$(".responder").click(function(){
-		$('.espacioComentario').fadeIn();
-	});
-
-	$(".cerrarComent").click(function(){
-		$('.espacioComentario').fadeOut();
-	});
-
-	/*OCULTAR VIDEO DESPUES DE TERMINADO*/
-	$('video').on('ended',function(){
-      	$('.mejs-cncMateriales').fadeOut();
-      	$('.nextVideo').fadeIn();
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
     });
-
-    $(".verNuevamente").click(function(){
-		$('.nextVideo').fadeOut();
-      	$('.mejs-cncMateriales').fadeIn();
-	});
-	$(".continuar").click(function(){
-		$('.nextVideo').fadeOut();
-      	$('.mejs-cncMateriales').fadeIn();
-	});
-
-	/*RESPONSIVE PAGINA INGENIERIA*/
-	var ancho = $(window).width();
-	if (ancho < 768) {
-		$('.icon').attr("src", "public/images/ingenierias/electronica/ingenieria_titulo_electronica.svg");
-	}
+  });
 });
-
-$(window).resize(function(){
-	var ancho = $(window).width();
-	if (ancho >767) {
-		$('.icon').attr("src", "public/images/ingenierias/electronica/ingenieria_icono_Electronica.svg");
-	}
-	else if (ancho < 768) {
-		$('.icon').attr("src", "public/images/ingenierias/electronica/ingenieria_titulo_electronica.svg");
-	}
-});	
 
 
 
