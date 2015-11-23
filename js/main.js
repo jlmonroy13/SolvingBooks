32c8de2b896c6399eda99e8a4f9b2133e6d4fcbb
Jorge  Luis Monroy Herrera <jlmonroy13@MacBook-Pro-de-Jorge.local>
2015-11-22 23:28:30 -0500

    markup home page listo, falta estilos Footer

--- js/main.js
+++ js/main.js
@@ -18,21 +18,46 @@ $(document).ready(function() {
 // HOME TRANSITION IMAGES
 $(document).ready(function(){
   var ancho = $(window).width();
-  if (ancho >767) {
-    var image = $('.image_slider'), i =1;
+  if (ancho >670) {
+    var image = $('.image-slider'), i =1;
     function refresh () {
       i++;
       if (i > 6) i = 1;
       image.fadeOut(1000, function () {
         image.css("background", "url('public/images/home/desktop/bg" + i + ".jpg') no-repeat center");
         image.css("background-size", "cover");
-        image.css("height", "91.03vh");
+        image.css("height", "90.7vh");
         image.css("max-height", "670px");
         image.css("position", "relative");
         image.fadeIn(1500);
       });
     }
     setInterval(refresh, 7500)
+  }
+}); 
+
+$(window).resize(function(){
+  var ancho = $(window).width();
+  if (ancho >670) {
+    var image = $('.image-slider'), i =1;
+
+    function refresh () {
+      i++;
+      if (i > 6) i = 1;
+      image.fadeOut(1000, function () {
+        image.css("background", "url('public/images/home/desktop/bg" + i + ".jpg') no-repeat center");
+        image.css("background-size", "cover");
+        image.css("height", "90.7vh");
+        image.css("max-height", "670px");
+        image.css("position", "relative");
+        image.fadeIn(1500);
+      });
+    }
+    setInterval(refresh, 7500)
+  }
+  else if ( ancho < 670) {
+    var image = $('.image-slider'), i =1;
+    image.stop();
   }
 }); 
 
