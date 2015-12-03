b06fbfe9193a6f128294724fb06f34bcb99e823c
Jorge  Luis Monroy Herrera <jlmonroy13@MacBook-Pro-de-Jorge.local>
2015-12-02 23:38:10 -0500

    markup de la parte del video lista, faltan estilos

--- js/main.js
+++ js/main.js
@@ -116,5 +116,25 @@ $('.js-accordion-trigger').bind('click', function(e){
   jQuery(this).parent().toggleClass('is-expanded');
   e.preventDefault();
 });
+//=========== VIDEO PAGE - TABS ACCORDION VIDEOS AND EXCERCISES ===============================
+$(document).ready(function () {
+  $('.accordion-tabs-minimal').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+  $('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs-minimal');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
+
 
 
