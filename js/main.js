47bb70422a41cc1775dca1d28a031e1e3f0a0508
Jorge  Luis Monroy Herrera <jlmonroy13@MacBook-Pro-de-Jorge.local>
2015-12-10 23:29:37 -0500

    markup tools video page

--- js/main.js
+++ js/main.js
@@ -130,6 +130,25 @@ jQuery(document).ready(function() {
         e.preventDefault();
     });
 });
+//=========== VIDEO PAGE - TABS TOOLS (BOURBON) ================================================
+$(document).ready(function () {
+  $('.accordion-tabs-minimal').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+  $('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs-minimal');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
 //===========OCULTAR VIDEO DESPUES DE TERMINADO ==========================================
 $(document).ready(function(){
   $('video').on('ended',function(){
@@ -158,6 +177,8 @@ $(document).ready(function(){
         e.preventDefault();
     });
 });
+
+
 
 
 
